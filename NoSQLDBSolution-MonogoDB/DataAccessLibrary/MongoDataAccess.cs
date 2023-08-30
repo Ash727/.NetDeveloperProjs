25808fa1aea5fa9ca5d6696ade070b0ef9a84bea
Ash727 <32463254+Ash727@users.noreply.github.com>
2023-08-29 21:54:38 -0400

    Add Initial MongoDbPRoject

---
+++ NoSQLDBSolution-MonogoDB/DataAccessLibrary/MongoDataAccess.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class MongoDataAccess
+    {
+        /*
+        NOTE A DATABASE IS A COLLECTION OF TABLES FOR NOSQL OR SQL 
+        IN MONGODB COLLECTION = TABLE = COLLECTION
+         */
+        public IMongoDatabase db;
+        public MongoDataAccess(string dbName, string connectionString)
+        {
+            var client = new MongoClient(connectionString); // Connection to cluster 
+            db = client.GetDatabase(dbName); // Connection to db 
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            // A Databas is a collection of tables 
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        public T LoadRecordById (string table, Guid id)
+        {
+
+        }
+
+    }
+}
---
+++ NoSQLDBSolution-MonogoDB/MonogDBUi/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MongoDBUI;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static void InitializeConfiguration(string connectionStringName)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+        var config  = builder.Build();
+        config.GetConnectionString(connectionStringName);
+    }
+}
