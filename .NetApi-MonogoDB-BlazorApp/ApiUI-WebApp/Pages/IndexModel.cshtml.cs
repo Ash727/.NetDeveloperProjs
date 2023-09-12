b207c19782e608ebd72641294c4dba089a0c020f
Ash727 <32463254+Ash727@users.noreply.github.com>
2023-09-12 18:15:09 -0400

    Projects corrected and placed in correct place api can retreieve data

---
+++ .NetApi-MonogoDB-BlazorApp/ApiUI-WebApp/Pages/IndexModel.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using ApiUI_WebApp.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+
+namespace SampleAPI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory; // IHttpclient factory is able to be used becuase of dependency injetion in the program.cs at builder.Services.AddHttpClient();
+
+
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async void OnGet()
+        {
+            await GetAllContacts();
+        }
+
+        private async Task GetAllContacts()
+        {
+            var _client = _httpClientFactory.CreateClient();
+            var response = await _client.GetAsync("https://localhost:7143/api/Contacts");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var options = new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true,
+
+                };
+
+                List<ContactModel> contacts;
+                string responseText = await response.Content.ReadAsStringAsync();
+                contacts = JsonSerializer.Deserialize<List<ContactModel>>(responseText, options);
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+
+        }
+    }
+}
---
+++ .NetApi-MonogoDB-BlazorApp/SampleAPI/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private static MongoDataAccess db;
+        private static string tableName = "Contacts"; // or the Collection cause Collection = table 
+        private static IConfiguration _config;
+        public ContactsController(IConfiguration config)
+        {
+            _config = config;
+            db = new MongoDataAccess("MongoContactsDB", _config.GetConnectionString("Default"));
+
+        }
+        [HttpGet]
+        public List<ContactModel> GetAll()
+        {
+            return db.LoadRecords<ContactModel>(tableName);
+        }
+ 
+    }
+}
