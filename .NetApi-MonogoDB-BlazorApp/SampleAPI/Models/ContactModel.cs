6551fc5af7eb35e2c9b0c7150747df604ad4af71
Ash727 <32463254+Ash727@users.noreply.github.com>
2023-09-12 16:21:12 -0400

    Web Ap connection to web API

---
+++ .NetApi-MonogoDB-BlazorApp/SampleAPI/Models/ContactModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiDBUi.Models
+{
+
+    public class ContactModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<EmailaddressModel> EmailAddresses { get; set; }
+        public List<PhoneNumberModel> PhoneNumbers { get; set; }
+    }
+}
